39d659365f4e05b47d5b917319245e962dbe957e
DevButani <devbutani26@gmail.com>
2025-03-29 20:03:24 +0530

    Updates

---
+++ Project/models.py
@@ -0,0 +1,69 @@
+from Project import db,app
+from flask_admin import Admin,AdminIndexView
+from flask import redirect,url_for,flash
+from flask_login import UserMixin,LoginManager,current_user
+from flask_admin.contrib.sqla import ModelView
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'signin'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(500), nullable=False, unique=True)
+    password = db.Column(db.String(1000), nullable=False)
+    
+    participations = db.relationship('Participate', backref='user', lazy=True)
+    memberships = db.relationship('Member', backref='user', lazy=True)
+    created_events = db.relationship('Event', backref='event_creator', lazy=True)
+
+class Event(db.Model):
+    event_id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    event_name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(1000))
+    version_number = db.Column(db.Integer, nullable=False)
+    creator = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.group_id'))
+    
+    participations = db.relationship('Participate', backref='event', lazy=True)
+
+class Group(db.Model):
+    group_id = db.Column(db.Integer, primary_key=True)
+    group_name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(1000))
+    version_number = db.Column(db.Integer, nullable=False)
+    
+    members = db.relationship('Member', backref='group', lazy=True)
+    events = db.relationship('Event', backref='host_group', lazy=True)
+
+class Participate(db.Model):
+    participate_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.event_id'))
+    status = db.Column(db.String(50), nullable=False)
+    
+    __table_args__ = (
+        db.CheckConstraint("status IN ('Accepted', 'Declined', 'Pending', 'NA')"),
+    )
+
+class Member(db.Model):
+    member_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.group_id'))
+    permission = db.Column(db.String(50), nullable=False)
+    
+    __table_args__ = (
+        db.CheckConstraint("permission IN ('Admin', 'Editor', 'Viewer')"),
+    )
