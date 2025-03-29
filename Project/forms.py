7709567f31fb05f7e47a79517e9e181946ee6533
Lakshya <jklagrawal123@gmail.com>
2025-03-29 20:02:12 +0530

    Updated SignIn and SignUp form

--- Project/forms.py
+++ Project/forms.py
@@ -1,8 +1,34 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField,TextAreaField,BooleanField
+from wtforms.validators import DataRequired,EqualTo,Email,Length,Regexp,Optional
 
+MIN_PASSWORD_LEN = 8
+MAX_PASSWORD_LEN = 12
 
-class Loginform(FlaskForm):
-    pass
+class SignUpForm(FlaskForm):
+    name = StringField("Name",validators=[DataRequired(), Length(max=200)])
+    email = StringField("Email",validators=[DataRequired(),
+                                            Email(message="Invalid email address (should be of the form something@example.com)"),
+                                            Length(max=500)])
+    password = PasswordField("Password",validators=[DataRequired(),
+                                                    Length(min=MIN_PASSWORD_LEN,max=MAX_PASSWORD_LEN),
+                                                    EqualTo('confirm_password',
+                                                            message="Password does not match to Confirm Password. Please retype"),
+                                                    Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]",
+                                                          message="Password should contain atleast one lowercase character, one uppercase character,one number and one special character." )])
+    confirm_password = PasswordField("Confirm-Password",validators=[DataRequired(),
+                                                                    Length(min=MIN_PASSWORD_LEN,max=MAX_PASSWORD_LEN)])
+    submit = SubmitField("Submit")
 
-class RegistrationForm(FlaskForm):
-    pass
+class SignInForm(FlaskForm):
+    email = StringField("Email",validators=[DataRequired(),
+                                            Email(message="Invalid email address (should be of the form something@example.com)"),
+                                            Length(max=500)])
+    password = PasswordField("Password",validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Submit")
+
+class GroupForm(FlaskForm):
+    name = StringField('Group Name', validators=[DataRequired(), Length(max=200)])
+    description = TextAreaField('Description', validators=[Optional(), Length(max=1000)])
+    submit = SubmitField('Save')
--- Project/routes.py
+++ Project/routes.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask,flash
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,login_required,current_user,logout_user
+from Project.forms import SignInForm,SignUpForm,GroupForm
 from flask import request, render_template, jsonify
 from Project import app
 import json
 import os
+
+@app.route('/signin')
+def signin():
+    form = SignInForm()
+    
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(email=form.email.data).first()
+    #     if user:
+    #         if check_password_hash(user.password,str(form.password.data)):
+    #             login_user(user,remember=form.remember_me.data)
+    #             flash("Logged in Successfully")
+    #             return render_template('base.html')
+    #         else:
+    #             flash("Wrong Password! Try Again",'error')
+    #     else:
+    #         flash("User not Found! Try Again",'error')
+    return render_template('signin.html',form=form,current_user=current_user)
+
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html',form=form)
 
 # Define the path to the events.json file
 EVENTS_FILE = os.path.join(os.path.dirname(__file__), 'events.json')
