5017396176619864fa710f92fbe8ef7b201bb926
Lakshya <jklagrawal123@gmail.com>
2025-03-29 20:13:56 +0530

    updated routes.py signin and signup

--- Project/routes.py
+++ Project/routes.py
@@ -2,32 +2,58 @@ from flask import Flask,flash
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import login_user,login_required,current_user,logout_user
 from Project.forms import SignInForm,SignUpForm,GroupForm
+from Project.models import User,Event,Group,Participate,Member
 from flask import request, render_template, jsonify
-from Project import app
+from Project import app,db
 import json
 import os
 
-@app.route('/signin')
+# To signin an existing user
+@app.route('/signin', methods=['GET','POST'])
 def signin():
     form = SignInForm()
     
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(email=form.email.data).first()
-    #     if user:
-    #         if check_password_hash(user.password,str(form.password.data)):
-    #             login_user(user,remember=form.remember_me.data)
-    #             flash("Logged in Successfully")
-    #             return render_template('base.html')
-    #         else:
-    #             flash("Wrong Password! Try Again",'error')
-    #     else:
-    #         flash("User not Found! Try Again",'error')
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if check_password_hash(user.password,str(form.password.data)):
+                login_user(user,remember=form.remember_me.data)
+                flash("Logged in Successfully")
+                return render_template('base.html')
+            else:
+                flash("Wrong Password! Try Again",'error')
+        else:
+            flash("User not Found! Try Again",'error')
     return render_template('signin.html',form=form,current_user=current_user)
 
-@app.route('/signup')
+# To create a new user
+@app.route('/signup', methods=['GET','POST'])
 def signup():
     form = SignUpForm()
-    return render_template('signup.html',form=form)
+    if form.validate_on_submit():
+        user_email = User.query.filter_by(email=form.email.data).first()
+
+        if (user_email is None):
+            newUser = User()
+            newUser.name = form.name.data
+            newUser.email = form.email.data
+            newUser.password = generate_password_hash(str(form.password.data))
+            try:
+                db.session.add(newUser)
+                db.session.commit()
+                login_user(newUser, remember=False)
+                flash("User Added Successfully")
+            except:
+                return "Unable to enter User to the Database"
+            form.name.data = ''
+            form.email.data = ''
+            form.password.data = ''
+            form.confirm_password.data = ''
+            return render_template('base.html')
+        else:
+            flash('This email already exits. Please sign in','error')
+            return render_template('signup.html',form=form)
+    return render_template('signup.html',form = form)
 
 # Define the path to the events.json file
 EVENTS_FILE = os.path.join(os.path.dirname(__file__), 'events.json')
