de7bea5cbedbe43d8fe55ff75eda821f5244b611
Lakshya <jklagrawal123@gmail.com>
2025-03-30 16:31:12 +0530

    UI updates

--- Project/static/js/dashboard.js
+++ Project/static/js/dashboard.js
@@ -1,6 +1,8 @@
 let btn = document.querySelector('#btn');
 let sidebar = document.querySelector('.sidebar');
 let searchBtn = document.querySelector('.bx-search');
+const groupdropdownToggles = document.querySelector('.group-dropdown-toggle');
+const groupdropdownmenu = document.querySelector('.group-dropdown-menu')
 
 btn.addEventListener('click', () => {
   sidebar.classList.toggle('active');
@@ -8,4 +10,11 @@ btn.addEventListener('click', () => {
 
 searchBtn.addEventListener('click', () => {
   sidebar.classList.toggle('active');
+});
+
+groupdropdownToggles.addEventListener('click', () => {
+  if (groupdropdownmenu.style.display === "block")
+    groupdropdownmenu.style.display = "none";
+  else 
+    groupdropdownmenu.style.display = "block";
 });
