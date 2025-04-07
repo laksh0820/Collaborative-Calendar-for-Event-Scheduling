25a56c15c50f84a23a6293d087bedca5a2ab3d07
@kmohanb2003 <kmohanb2003@kgpian.iitkgp.ac.in>
2025-04-07 15:24:58 +0530

    Event invite decline fix

--- Project/routes.py
+++ Project/routes.py
@@ -202,11 +202,13 @@ def check_invites():
         try:
             if response['invite_type'] == 'group':
                 invite = Member.query.filter_by(member_id=response['invite_id']).first()
+                if response['status'] == 'Declined':
+                    db.session.delete(invite)
+                else:
+                    invite.status = response['status']
+                    invite.read_status = 'Read'
             else:
                 invite = Participate.query.filter_by(participate_id=response['invite_id']).first()
-            if response['status'] == 'Declined':
-                db.session.delete(invite)
-            else:
                 invite.status = response['status']
                 invite.read_status = 'Read'
             db.session.commit()
